1283dd6a10351dc6e13fdde630aa37412e28fe6d
Anton Kodochygov <bricks@hotmail.com>
2016-08-12 00:52:24 -0500

    add some more sorting stuff ...

--- utils/data/data.h
+++ utils/data/data.h
@@ -58,6 +58,79 @@ void write_file ( std::string                         file_name
     }
 }
 
+void sort_write_file ( std::string                                         initial_name
+                     , std::vector < std::vector < std::string > > const & sorted_data
+                     , std::string                                         comprehensive_name
+                     )
+{
+    std::string line;
+    std::ifstream input (initial_name.c_str());
+    std::ofstream output (comprehensive_name.c_str());
+    std::vector < std::vector < std::string > :: const_iterator > it_vec;
+    for ( std::size_t k(0)
+        ; k < sorted_data.size()
+        ; ++k
+        )
+    {
+        it_vec . push_back ( sorted_data[k] . begin () );
+    }
+    if (input.is_open())
+    {
+        if (output.is_open())
+        {
+            while(true)
+            {
+                bool done ( true );
+                bool init ( true );
+                if(!input.eof())
+                {
+                    input >> line;
+                }
+                {
+                    for ( std::size_t k(0)
+                        ; k < it_vec.size()
+                        ; ++k
+                        )
+                    {
+                        if ( it_vec[k] != sorted_data[k] . end () )
+                        {
+                            if ( init )
+                            {
+                                line = *(it_vec[k]);
+                                done = false;
+                                init = false;
+                            }
+                            else
+                            {
+                                if ( *(it_vec[k]) < line )
+                                {
+                                    line = *(it_vec[k]);
+                                    done = false;
+                                }
+                            }
+                        }
+                    }
+                }
+                if ( done )
+                {
+                    break;
+                }
+                output << line << " ";
+            }
+            output.close();
+        } 
+        else
+        {
+            std::cout << "Unable to open file : " << comprehensive_name << std::endl;
+        }
+        input.close();
+    }
+    else
+    {
+        std::cout << "Unable to open file : " << initial_name << std::endl;
+    }
+}
+
 void get_files ( std::string                   dir
                , std::vector < std::string > & files
                )
@@ -124,11 +197,30 @@ void sort_directory ( std::string dir
 // it is assumed to already contain sorted data
 // the second input is a smaller vector in memory
 // the smaller vector is sorted in memory
-void sort_files ( std::string                         comprehensive_file
-                , std::vector < std::string > const & merging_input
+void sort_files ( std::string dir
+                , std::string initial_file
+                , std::string comprehensive_file
                 )
 {
-
+    std::vector < std::string > files;
+    get_files ( dir , files );
+    if ( files . size () > 0 )
+    {
+        std::vector < std::vector < std::string > > sorted_data;
+        for ( std::size_t k(0)
+            ; k < files.size()
+            ; ++k
+            )
+        {
+            sorted_data . push_back ( std::vector < std::string > () );
+            read_file ( files[k] , sorted_data[k] );
+            sort_data ( sorted_data[k] );
+        }
+        sort_write_file ( initial_file 
+                        , sorted_data
+                        , comprehensive_file
+                        );
+    }
 }
 
 #endif
