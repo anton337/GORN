b6f0e3de136c122cfb32c476209dd6b642697e2d
Anton Kodochygov <anton.vla.kodochygov@hpe.com>
2016-08-18 20:38:27 -0400

    adding distributed queue ...

---
+++ unit_tests/test_queue.cpp
@@ -0,0 +1,114 @@
+#include "unit_test.h"
+
+#include <iostream>
+
+#include "data/queue_file.h"
+#include <boost/thread.hpp>
+
+bool test_file_queue ()
+{
+    QueueFile queue_file ( "unit_tests/queue_data/" );
+    std::vector < QueueEntry > vec;
+    {
+        vec . push_back ( QueueEntry ( 1 , "hello"  ) );
+        vec . push_back ( QueueEntry ( 2 , "how"    ) );
+        vec . push_back ( QueueEntry ( 3 , "are"    ) );
+        vec . push_back ( QueueEntry ( 4 , "you"    ) );
+        vec . push_back ( QueueEntry ( 5 , "?"      ) );
+    }
+    queue_file . Push ( "file1" , vec );
+    std::vector < QueueEntry > out_vec;
+    queue_file . Pop ( out_vec );
+    for ( std::size_t k(0)
+        ; k < out_vec . size ()
+        ; ++k
+        )
+    {
+        std::cout << out_vec[k].key << " " << out_vec[k].value << std::endl;
+    }
+    return true;
+}
+
+void pop ( std::vector < QueueEntry > vec
+         , QueueFile                  queue_file
+         )
+{
+    queue_file . Pop ( vec );
+}
+
+void push ( std::vector < QueueEntry > vec
+          , std::string                file_name
+          , QueueFile                  queue_file
+          )
+{
+    queue_file . Push ( file_name , vec );
+}
+
+bool test_file_queue_parallel_test ()
+{
+    std::size_t n_threads = 20;
+    QueueFile queue_file ( "unit_tests/queue_data/" );
+    std::vector < boost::thread * > threads;
+    std::vector < QueueEntry > vec;
+    for ( std::size_t k(0)
+        ; k < 10000
+        ; ++k
+        )
+    {
+        vec . push_back ( QueueEntry ( k , "hello"  ) );
+    }
+    std::cout << "push" << std::endl;
+    for ( std::size_t file(0)
+        ; file < n_threads
+        ; ++file
+        )
+    {
+        std::stringstream ss;
+        ss << "file-" << file << ".que";
+        threads . push_back ( new boost::thread ( push 
+                                                , vec 
+                                                , ss.str() 
+                                                , queue_file 
+                                                ) 
+                            );
+    }
+    std::vector < QueueEntry > out_vec;
+    std::cout << "pop" << std::endl;
+    for ( std::size_t file(0)
+        ; file < n_threads
+        ; ++file
+        )
+    {
+        threads . push_back ( new boost::thread ( pop 
+                                                , out_vec 
+                                                , queue_file 
+                                                ) 
+                            );
+    }
+    for ( std::size_t k(0)
+        ; k < threads . size ()
+        ; ++k
+        )
+    {
+        threads[k] -> join ();
+    }
+    std::cout << "done" << std::endl;
+    for ( std::size_t k(0)
+        ; k < out_vec . size ()
+        ; ++k
+        )
+    {
+        std::cout << out_vec[k].key << " " << out_vec[k].value << std::endl;
+    }
+    return true;
+}
+
+int main()
+{
+    srand ( time ( NULL ) );
+    std::cout << "Welcome to File Queue Unit Test!" << std::endl;
+    UnitTest("Test file queue",test_file_queue);
+    UnitTest("Test file queue parallel test",test_file_queue_parallel_test);
+    return 0;
+}
+
---
+++ utils/data/queue_file.h
@@ -0,0 +1,215 @@
+#ifndef QUEUE_FILE_H
+#define QUEUE_FILE_H
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include "boost/filesystem.hpp"
+
+#include "data.h"
+
+struct QueueEntry
+{
+    std::size_t key;
+    std::string value;
+    QueueEntry ( std::size_t _key
+               , std::string _value
+               )
+    : key   ( _key   )
+    , value ( _value )
+    {
+
+    }
+};
+
+class QueueFile
+{
+    std::string m_directory_name;
+    std::string m_unlock_name;
+    std::string m_lock_name;
+public:
+    QueueFile ( std::string p_directory_name )
+    : m_directory_name ( p_directory_name )
+    , m_unlock_name ( p_directory_name+"/../unlocked" )
+    , m_lock_name ( p_directory_name+"/../locked" )
+    {
+
+    }
+
+    ~QueueFile ()
+    {
+
+    }
+
+    void Push ( std::string                        filename
+              , std::vector < QueueEntry > const & input 
+              )
+    {
+        bool status = false;
+        while ( !status )
+        {
+            if ( push ( m_directory_name + "/" + filename
+                      , input 
+                      )
+                 == 0 
+               )
+            {
+                status = true;
+                continue;
+            }
+            sleep(5);
+        }
+    }
+
+    void Pop ( std::vector < QueueEntry > & output )
+    {
+        bool status = false;
+        while ( !status )
+        {
+            std::vector < std::string > files;
+            get_files ( m_directory_name 
+                      , files 
+                      );
+            if ( files . size () > 0 )
+            {
+                if ( pop ( files[0] 
+                         , output 
+                         )
+                     == 0 
+                   )
+                {
+                    status = true;
+                    continue;
+                }
+            }
+            sleep(1);
+        }
+    }
+
+private:
+
+    int lock ()
+    {
+        if ( boost::filesystem::exists ( m_lock_name ) )
+        {
+            // directory locked, move along
+            return 1;
+        }
+        else
+        {
+            // lock directory
+            // boost::filesystem::rename(src,target)
+            // should be atomic
+            // so this should be equivalent to locking a mutex
+            try
+            {
+                boost::filesystem::rename ( m_unlock_name , m_lock_name );
+            }
+            catch ( ... )
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+
+    int unlock ()
+    {
+        if ( boost::filesystem::exists ( m_unlock_name ) )
+        {
+            // can't unlock a mutex that is not locked, 
+            // this should be an error,
+            // it seems the developer messed up
+            std::cout << "can't unlock a mutex that is not locked" << std::endl;
+            return 1;
+        }
+        else
+        {
+            // unlock directory
+            // boost::filesystem::rename(src,target)
+            // should be atomic
+            // so this should be equivalent to unlocking a mutex
+            try
+            {
+                boost::filesystem::rename ( m_lock_name , m_unlock_name );
+            }
+            catch ( ... )
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+
+    int push ( std::string                        file_name
+             , std::vector < QueueEntry > const & input
+             )
+    {
+        // if directory is locked, 
+        // we don't allow the thread to write to this file
+        if ( lock () == 1 ) return 1;
+        std::ofstream myfile (file_name.c_str());
+        if (myfile.is_open())
+        {
+            for ( std::size_t k(0)
+                ; k < input.size()
+                ; ++k
+                )
+            {
+                myfile << input[k].key   << " "
+                       << input[k].value << std::endl;
+            }
+            myfile.close();
+        } 
+        else
+        {
+            std::cout << "Unable to open file : " << file_name << std::endl;
+        }
+        Unlock();
+        return 0;
+    }
+
+    int pop ( std::string                  file_name
+            , std::vector < QueueEntry > & output
+            )
+    {
+        // since we are going to remove the file, once it is in memory,
+        // and we don't want other threads to contain duplicate data,
+        // we want to lock the directory before reading this file
+        if ( lock () == 1 ) return 1;
+        std::string line;
+        std::ifstream myfile (file_name.c_str());
+        if (myfile.is_open())
+        {
+            while ( getline ( myfile , line ) )
+            {
+                std::stringstream ss;
+                ss << line;
+                std::size_t key;
+                ss >> key;
+                std::string value;
+                ss >> value;
+                output . push_back ( QueueEntry ( key , value ) );
+            }
+            myfile.close();
+        }
+        else
+        {
+            std::cout << "Unable to open file : " << file_name << std::endl; 
+        }
+        boost::filesystem::remove ( file_name );
+        Unlock();
+        return 0;
+    }
+
+    void Unlock()
+    {
+        while ( unlock () == 1 ) { sleep ( 1 ); }
+    }
+};
+
+#endif
+
